da47c9cfbefee214c00354e22614774cc1851956
Gustavo Teixeira <iwhrim@gmail.com>
2021-04-30 20:57:52 -0300

    First commit of the application

---
+++ src/main/java/dev/gustavoteixeira/api/votingsession/VotingSessionApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira.api.votingsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gustavoteixeira/api/votingsession/VotingSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira.api.votingsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

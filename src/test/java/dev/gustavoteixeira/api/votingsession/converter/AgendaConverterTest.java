49f867a078a04d8be1cd48055cbceb6b8e2b69ab
Gustavo Teixeira <iwhrim@gmail.com>
2021-05-01 23:40:28 -0300

    100% code coverage on converter layer

---
+++ src/test/java/dev/gustavoteixeira/api/votingsession/converter/AgendaConverterTest.java
@@ -0,0 +1,79 @@
+package dev.gustavoteixeira.api.votingsession.converter;
+
+import dev.gustavoteixeira.api.votingsession.dto.response.AgendaResponseDTO;
+import dev.gustavoteixeira.api.votingsession.entity.Agenda;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static dev.gustavoteixeira.api.votingsession.converter.AgendaConverter.getAgendaResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class AgendaConverterTest {
+
+    public static final String AGENDA_ID = "608d817df3117478ca0f7432";
+    public static final String AGENDA_NAME = "Rising gasoline tax by 3%";
+    public static final int AGENDA_DURATION = 2;
+
+    @Test
+    public void getAgendaResponseWithNewlyCreatedShouldReturnAgendaResponseDTO() {
+        Agenda agenda = Agenda.builder()
+                .id(AGENDA_ID)
+                .name(AGENDA_NAME)
+                .duration(AGENDA_DURATION).build();
+
+        AgendaResponseDTO result = getAgendaResponse(agenda);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(AGENDA_ID);
+        assertThat(result.getName()).isEqualTo(AGENDA_NAME);
+        assertThat(result.getDuration()).isEqualTo(AGENDA_DURATION);
+        assertThat(result.getStartTime()).isNull();
+        assertThat(result.getPositiveVotes()).isZero();
+        assertThat(result.getNegativeVotes()).isZero();
+        assertThat(result.isOpened()).isFalse();
+    }
+
+    @Test
+    public void getAgendaResponseWithAgendaVotingInProgressShouldReturnAgendaResponseDTO() {
+        Agenda agenda = Agenda.builder()
+                .id(AGENDA_ID)
+                .name(AGENDA_NAME)
+                .duration(AGENDA_DURATION)
+                .startTime(LocalDateTime.now().minusMinutes(1)).build();
+
+        AgendaResponseDTO result = getAgendaResponse(agenda);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(AGENDA_ID);
+        assertThat(result.getName()).isEqualTo(AGENDA_NAME);
+        assertThat(result.getDuration()).isEqualTo(AGENDA_DURATION);
+        assertThat(result.getStartTime()).isNotNull();
+        assertThat(result.getPositiveVotes()).isZero();
+        assertThat(result.getNegativeVotes()).isZero();
+        assertThat(result.isOpened()).isTrue();
+    }
+
+    @Test
+    public void getAgendaResponseWithAlreadyVotedAgendaShouldReturnAgendaResponseDTO() {
+        Agenda agenda = Agenda.builder()
+                .id(AGENDA_ID)
+                .name(AGENDA_NAME)
+                .duration(AGENDA_DURATION)
+                .startTime(LocalDateTime.now().minusMinutes(2)).build();
+
+        AgendaResponseDTO result = getAgendaResponse(agenda);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(AGENDA_ID);
+        assertThat(result.getName()).isEqualTo(AGENDA_NAME);
+        assertThat(result.getDuration()).isEqualTo(AGENDA_DURATION);
+        assertThat(result.getStartTime()).isNotNull();
+        assertThat(result.getPositiveVotes()).isZero();
+        assertThat(result.getNegativeVotes()).isZero();
+        assertThat(result.isOpened()).isFalse();
+    }
+
+}
---
+++ src/test/java/dev/gustavoteixeira/api/votingsession/converter/VoteConverterTest.java
@@ -0,0 +1,33 @@
+package dev.gustavoteixeira.api.votingsession.converter;
+
+import dev.gustavoteixeira.api.votingsession.dto.response.VoteResponseDTO;
+import dev.gustavoteixeira.api.votingsession.entity.Vote;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static dev.gustavoteixeira.api.votingsession.converter.VoteConverter.getVoteResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class VoteConverterTest {
+
+    public static final String AGENDA_ID = "608d817df3117478ca0f7432";
+    public static final String ASSOCIATE_IDENTIFIER = "38347541027";
+    public static final String POSITIVE_CHOICE = "SIM";
+
+    @Test
+    public void getAgendaResponseWithNewlyCreatedShouldReturnAgendaResponseDTO() {
+        Vote vote = Vote.builder()
+                .agendaId(AGENDA_ID)
+                .associate(ASSOCIATE_IDENTIFIER)
+                .choice(POSITIVE_CHOICE).build();
+
+        VoteResponseDTO result = getVoteResponse(vote);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getAgendaId()).isEqualTo(AGENDA_ID);
+        assertThat(result.getAssociate()).isEqualTo(ASSOCIATE_IDENTIFIER);
+        assertThat(result.getChoice()).isEqualTo(POSITIVE_CHOICE);
+    }
+
+}
